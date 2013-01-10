378bc5946b4f996365e43f901ecbfe5854b52c5c
Eitan Romanoff <ear7631@gmail.com>
2013-01-10 15:11:34 -0500

    Command line arguments for the test program

--- TestProgram.java
+++ TestProgram.java
@@ -1,9 +1,9 @@
 public class TestProgram {
 
-    public static void uniformTest() {
-        Manet network = new UniformManet(1234);
+    public static void uniformTest(int num_nodes, long seed) {
+        Manet network = new UniformManet(seed);
 
-        for(int i = 0; i < 500; i++) {
+        for(int i = 0; i < num_nodes; i++) {
             network.generateNode();
         }
 
@@ -13,10 +13,10 @@ public class TestProgram {
     }
 
 
-    public static void poissonTest() {
-        Manet network = new PoissonManet(1234);
+    public static void poissonTest(int num_nodes, long seed) {
+        Manet network = new PoissonManet(seed);
 
-        for(int i = 0; i < 500; i++) {
+        for(int i = 0; i < num_nodes; i++) {
             network.generateNode();
         }
 
@@ -26,7 +26,31 @@ public class TestProgram {
 
 
     public static void main(String args[]) {
-        uniformTest();
-        poissonTest();
+        if (args.length != 3) {
+            usage();
+            System.exit(1);
+        }
+
+        try {
+            long seed = Long.parseLong(args[2]);
+            int num_nodes = Integer.parseInt(args[1]);
+            if (args[0].equals("poisson")) {
+                poissonTest(num_nodes, seed);
+            }
+            else if (args[0].equals("uniform")) {
+                uniformTest(num_nodes, seed);
+            }
+            else {
+                usage();
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            usage();
+            System.exit(1);
+        }
+    }
+
+    public static void usage() {
+        System.out.println("Usage: java TestProgram.java <gen_type> <num_nodes> <gen_seed>");
     }
 }
