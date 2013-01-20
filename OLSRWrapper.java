e0b0553f78b6ed9aa86f2921e7fbca826fe11a18
Eitan Romanoff <ear7631@gmail.com>
2013-01-20 14:54:36 -0500

    Implemented getMPRs fully

--- OLSRWrapper.java
+++ OLSRWrapper.java
@@ -23,16 +23,50 @@ public class OLSRWrapper {
 
         // Generate N2
         for (Node n : n_one ) {
-            if (n != source) {
-                n_two.add(n);
+            for (Node neighbor : n.getNeighbors()) {
+                if (neighbor != source) {
+                    n_two.add(neighbor);
+                }
             }
         }
 
+        coverage.add(source);
+        coverage.addAll(n_one);
+
         // Single-neighbor nodes in N2
-        for (Node n : n_two) {
-            if (n.numNeighbors() == 1) {
-                coverage.add(n);
+        for (Node n : n_one) {
+            for (Node neighbor : n.getNeighbors()) {
+                if (neighbor.numNeighbors() == 1) {
+                    coverage.add(neighbor);
+                    selectedMPRs.add(n);
+                }
             }
+        }
+
+        while (!coverage.containsAll(n_two)) {
+
+            // Go through all remaining in the N1 layer, find the one with the
+            // most amount of uncovered neighbors
+            HashSet<Node> remaining = new HashSet<Node>(n_one);
+            remaining.removeAll(selectedMPRs);
+
+            Node maximum = null;
+            int max_num = 0;
+
+            for (Node n : remaining) {
+                if (maximum == null) { maximum = n; }
+
+                HashSet<Node> neighbors_clone = new HashSet<Node>(n.getNeighbors());
+                neighbors_clone.removeAll(coverage);
+
+                if (neighbors_clone.size() > max_num) {
+                    maximum = n;
+                    max_num = neighbors_clone.size();
+                }
+            }
+
+            selectedMPRs.add(maximum);
+            coverage.addAll(maximum.getNeighbors());
         }
 
         return selectedMPRs;
