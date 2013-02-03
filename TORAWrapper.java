e6af4a669d349167fa0a6a6f0a0c8aaee2f8ee34
Andrew <Andrew@Trogdor.student.rit.edu.>
2013-02-03 15:26:14 -0500

    TORA source to destination functionality

--- TORAWrapper.java
+++ TORAWrapper.java
@@ -1,6 +1,11 @@
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+
+import edu.rit.numeric.ListXYSeries;
+import edu.rit.numeric.plot.Plot;
 
 
 public class TORAWrapper extends ManetWrapper {
@@ -15,7 +20,7 @@ public class TORAWrapper extends ManetWrapper {
     //Map of each node's route_request bit. Each node stores an RR bit for each 
     //	possible destination node in the network.
     //	<source, <destination, set/not>>
-    HashMap<Node, HashMap<Node, Boolean>> routeReq_Dest_bit;
+    HashMap<Node, HashSet<Node>> routeReq_Dest_bit;
 	
 	
     public TORAWrapper(Manet network) {
@@ -27,15 +32,23 @@ public class TORAWrapper extends ManetWrapper {
     	this.UPD_rec_counter = new HashMap<Node, Integer>();
     	
     	//Represents the RRbit, true=node already received qry; false=not received qry
-    	this.route_required_Bit = new HashMap<Node, Boolean>();
-    	this.routeReq_Dest_bit = new HashMap<Node, HashMap<Node, Boolean>>();
+//    	this.route_required_Bit = new HashMap<Node, Boolean>();
     	
+    	this.routeReq_Dest_bit = new HashMap<Node, HashSet<Node>>();
+    	for (Node node : network) {
+			this.routeReq_Dest_bit.put(node, new HashSet<Node>());
+			this.QRY_rec_counter.put(node, 0);
+			this.QRY_sent_counter.put(node, 0);
+			this.UPD_rec_counter.put(node, 0);
+			this.UPD_sent_counter.put(node, 0);
+		}
     }
 
 
-    public HashSet<Node> findPathToDestination(Node source, Node destination) {
+//    public HashSet<Node> findPathToDestination(Node source, Node destination) {
+    public LinkedHashSet<Node> findPathToDestination(Node source, Node destination) {
     	
-    	HashSet<Node> destPath = new HashSet<Node>();
+    	LinkedHashSet<Node> destPath = new LinkedHashSet<Node>();
     	
     	//Check Neighbors; send QRY packet
 //    	HashSet<Node> neighbors = source.getNeighbors();
@@ -44,10 +57,22 @@ public class TORAWrapper extends ManetWrapper {
 //			source.sendQRY(neighbor);	//TODO add sendQRY function
 //		}
     	
+    	System.out.println("Destination: " + destination.toString());
     	//send QRY to all neighbor-neighbors:
-    	recurseQueries(source, destination);
+    	destPath = recurseQueries(source, destination);
+    	
+    	if(destPath == null) {
+    		//No path exists to destination
+    	}
+    	else {
+    		//
+    	}
+    	
     	//Set the source bit of the original node
-    	this.route_required_Bit.put(source, true);
+    	this.routeReq_Dest_bit.get(source).add(destination);
+//    	this.route_required_Bit.put(source, destination);
+    	
+    	
     	
     	//Receiving update packet, add to destPath.
     	
@@ -56,58 +81,78 @@ public class TORAWrapper extends ManetWrapper {
     }
     
     
-    public HashSet<Node> recurseQueries(Node source, Node destination) {
+    public LinkedHashSet<Node> recurseQueries(Node source, Node destination) {
     	
     	//Check Neighbors; 
     	HashSet<Node> neighbors = source.getNeighbors();
     	//HashSet to hold all neighbors which have their own neighbors
     	HashSet<Node> newSources = new HashSet<Node>();
+    	//HashSet to hold backrack process from dest > source
+    	LinkedHashSet<Node> backtrack = new LinkedHashSet<Node>();
     	
+    	System.out.println("Called: " + source.toString());
     	if (source == destination) {
-    		
+    		//Start backtrack process
+    		backtrack.add(source);
+    		return backtrack;
     	}
-    	
-    	if (!neighbors.isEmpty()) {
-    		if (hasUnmetNeighbors(source) == true) {
-    			
-    			//Check if isLastNeighbor, start sending UPD
-    			
-		    	for (Node neighbor : neighbors) {
-					if (sendQuery(source, neighbor) == true) {
-						//add to HashSet to flood to child nodes
-						newSources.add(neighbor);
-					}
-					else {
-						//Node already received QRY
-					}
-		    	}
-		    	
-		    	//loop through
-		    	for (Node newNeighbor : newSources) {
-					recurseQueries(newNeighbor, destination);
-				}
+    	else {
+
+    		if (!neighbors.isEmpty()) {
+    			if (hasUnmetNeighbors(source, destination) == true) {
+
+    				//Check if isLastNeighbor, start sending UPD
+
+    				for (Node neighbor : neighbors) {
+    					if (sendQuery(source, destination, neighbor) == true) {
+    						//add to HashSet to flood to child nodes
+    						newSources.add(neighbor);
+    						System.out.println("new neighbors: " + neighbor.toString());
+    					}
+    					else {
+    						//Node already received QRY
+    					}
+    				}
+
+    				//loop through the hashset created above:
+    				for (Node newNeighbor : newSources) {
+    					System.out.println("calling neighbors: " + newNeighbor.toString());
+    					backtrack =  recurseQueries(newNeighbor, destination);
+    					if(backtrack == null) {
+//    						return null;
+    					}
+    					else {
+    						backtrack.add(newNeighbor);
+    						return backtrack;
+    					}
+
+    				}
+    			}
     		}
     	}
     	
-    	return neighbors;
+//    	return neighbors;
+    	return null;
+    	
     }
     
     
-    public boolean hasUnmetNeighbors(Node source) {
+    public boolean hasUnmetNeighbors(Node source, Node destination) {
     	//This function checks if a node has any neighbors who havent' 
-    	//	hear the qry.
+    	//	heard the qry.
     	
     	//Get Neighbors; 
     	HashSet<Node> neighbors = source.getNeighbors();
     	//Set to hold all non RRbit nodes
     	HashSet<Node> newNodes = new HashSet<Node>();
-    	boolean unmetNeighborFlag = true;
+    	boolean unmetNeighborFlag = false;
     	
     	if (!neighbors.isEmpty()) {
 	    	for (Node neighbor : neighbors) {
 				
-	    		if (this.route_required_Bit.get(neighbor) == false) {
-	    			//At least 1 unmet neighbor exists
+	    		//If the nodes are held within HashMap<neighbor, HashSet<destination>>
+	    		//Then they have already been visited by a qry
+	    		if (!this.routeReq_Dest_bit.get(neighbor).contains(destination)) {
 	    			unmetNeighborFlag = true;
 	    		}
 			}
@@ -135,15 +180,19 @@ public class TORAWrapper extends ManetWrapper {
      * @param neighbor
      * @return
      */
-    public boolean sendQuery(Node source, Node neighbor) {
+    public boolean sendQuery(Node source, Node destination, Node neighbor) {
     	
-    	boolean RRbit = this.route_required_Bit.get(neighbor); 
+    	boolean RR_alreadyAsked = false;
+    	if (this.routeReq_Dest_bit.get(neighbor).contains(destination)) {
+    		RR_alreadyAsked = true;
+    	}
+    		
     	
     	//send query from source node to it's neighbor
     	//Check if QRY RR bit is set: sendQRY returns true if already sent, false if not
     	
     	//Check if RR bit is set; if already set ignore this qry message:
-    	if (RRbit == false) {
+    	if (RR_alreadyAsked == false) {
     	
 	    	//increment Qry sent counter
 	    	this.QRY_sent_counter.put(source, (this.QRY_sent_counter.get(source)+1));
@@ -151,7 +200,8 @@ public class TORAWrapper extends ManetWrapper {
 	    	this.QRY_rec_counter.put(source, (this.QRY_rec_counter.get(source)+1));
 	    	
 	    	//Set RR flag to prevent duplicate Queries
-	    	this.route_required_Bit.put(source, true);
+//	    	this.route_required_Bit.put(source, true);
+	    	this.routeReq_Dest_bit.get(neighbor).add(destination);
 	    	
 	    	//Return true because route bit was not already set
 	    	return true;
@@ -163,6 +213,41 @@ public class TORAWrapper extends ManetWrapper {
     	}
     }
 
+    
+    public void showLink (Node source, Node dest) {
+    	//Print the linked source to dest nodes:
+    	
+    	LinkedHashSet<Node> StoDpath = new LinkedHashSet<Node>();
+    	
+    	StoDpath = findPathToDestination(source, dest);
+    	System.out.println();
+    	System.out.println();
+    	System.out.println();
+    	
+    	String result = "Source: " + source.getX() + ", " + source.getY() + "\n";
+    	//Display the path
+    	for (Node jumpNode : StoDpath) {
+			result += "(" + jumpNode.getX() + ", " + jumpNode.getY() + ")\n";
+		}
+    	result += "Destination: " + dest.getX() + ", " + dest.getY() + "\n"; 
+    	
+    	System.out.println(result);
+    	
+    	int totQRY = 0;
+    	int totUPD = 0;
+    	
+    	for (Node node : this.network) {
+			totQRY += this.QRY_sent_counter.get(node).intValue();
+		}
+    	for (Node node : this.network) {
+			totQRY += this.UPD_sent_counter.get(node).intValue();
+		}
+    	
+    	System.out.println("Total QRY packets sent: " + totQRY);
+    	System.out.println("Total UPD packet sent: " + totUPD);
+    }
+    
+    
 
 	@Override
 	public void floodPing() {
